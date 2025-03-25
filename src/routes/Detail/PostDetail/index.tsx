f45da8dc2b106caf311bbb12d8ea0c1f1412650a
soowan <nick6767676767@gmail.com>
2025-03-25 18:44:04 +0900

    slug css 수정 테스트

--- src/routes/Detail/PostDetail/index.tsx
+++ src/routes/Detail/PostDetail/index.tsx
@@ -1,62 +1,163 @@
-import React from "react"
-import PostHeader from "./PostHeader"
-import Footer from "./PostFooter"
-import CommentBox from "./CommentBox"
-import Category from "src/components/Category"
+import dynamic from "next/dynamic"
+import Image from "next/image"
+import Link from "next/link"
+import { ExtendedRecordMap, CodeBlock, Block as NotionBlock } from "notion-types"
+import useScheme from "src/hooks/useScheme"
+
+// core styles shared by all of react-notion-x (required)
+import "react-notion-x/src/styles.css"
+
+// used for code syntax highlighting (optional)
+import "prismjs/themes/prism-tomorrow.css"
+
+// used for rendering equations (optional)
+
+import "katex/dist/katex.min.css"
+import { FC, ComponentType, HTMLAttributes, ReactNode } from "react"
 import styled from "@emotion/styled"
-import NotionRenderer from "../components/NotionRenderer"
-import usePostQuery from "src/hooks/usePostQuery"
 
-type Props = {}
+const _NotionRenderer = dynamic(
+  () => import("react-notion-x").then((m) => m.NotionRenderer),
+  { ssr: false }
+)
 
-const PostDetail: React.FC<Props> = () => {
-  const data = usePostQuery()
+const Code = dynamic(() =>
+  import("react-notion-x/build/third-party/code").then(async (m) => m.Code)
+)
 
-  if (!data) return null
+const Collection = dynamic(() =>
+  import("react-notion-x/build/third-party/collection").then(
+    (m) => m.Collection
+  )
+)
+const Equation = dynamic(() =>
+  import("react-notion-x/build/third-party/equation").then((m) => m.Equation)
+)
+const Pdf = dynamic(
+  () => import("react-notion-x/build/third-party/pdf").then((m) => m.Pdf),
+  {
+    ssr: false,
+  }
+)
+const Modal = dynamic(
+  () => import("react-notion-x/build/third-party/modal").then((m) => m.Modal),
+  {
+    ssr: false,
+  }
+)
 
-  const category = (data.category && data.category?.[0]) || undefined
+const mapPageUrl = (id: string) => {
+  return "https://www.notion.so/" + id.replace(/-/g, "")
+}
+
+interface CustomCodeProps {
+  block: CodeBlock; //  CodeBlock 타입 사용
+  className?: string;
+  defaultLanguage?: string;
+}
+
+interface BlockProps {
+  block: NotionBlock;
+  children?: ReactNode;
+  className?: string;
+  bodyClassName?: string;
+  header?: React.ReactNode;
+  footer?: React.ReactNode;
+  pageHeader?: React.ReactNode;
+  pageFooter?: React.ReactNode;
+  pageTitle?: React.ReactNode;
+  pageAside?: React.ReactNode;
+  pageCover?: React.ReactNode;
+  hideBlockId?: boolean;
+  disableHeader?: boolean;
+  level?: number;
+}
+
+const CustomBlock: FC<BlockProps> = ({ block, children, className }) => {
+  const headingStyle = {
+    width: '100%',
+    display: 'block',
+    backgroundColor: block?.format?.block_color || 'transparent',
+  };
+
+  let Tag:any = 'h1'
+  if (block.type === 'sub_header') {
+    Tag = 'h2'
+  } else if (block.type === 'sub_sub_header') {
+    Tag = 'h3'
+  }
 
   return (
-    <StyledWrapper>
-      <article>
-        {category && (
-          <div css={{ marginBottom: "0.5rem" }}>
-            <Category readOnly={data.status?.[0] === "PublicOnDetail"}>
-              {category}
-            </Category>
-          </div>
-        )}
-        {data.type[0] === "Post" && <PostHeader data={data} />}
-        <div>
-          <NotionRenderer recordMap={data.recordMap} components={{}}/> {/* components prop 추가 */}
-        </div>
-        {data.type[0] === "Post" && (
-          <>
-            <Footer />
-            <CommentBox data={data} />
-          </>
-        )}
-      </article>
-    </StyledWrapper>
+    <div style={headingStyle}>
+      <Tag className={className}>
+        {block.properties?.title?.[0]?.[0]}
+      </Tag>
+      {children}
+    </div>
+  );
+};
+
+// react-notion-x 타입 확장
+declare module 'react-notion-x' {
+  interface NotionComponents {
+    Block: FC<BlockProps>;
+  }
+}
+
+type Props = {
+  recordMap: ExtendedRecordMap;
+  components: {
+    Code?: ComponentType<CustomCodeProps>;
+    Collection?: any;
+    Equation?: any;
+    Modal?: any;
+    Pdf?: any;
+    nextImage?: any;
+    nextLink?: any;
+    Block?: FC<BlockProps>; // 추가된 Block 타입
+  };
+  darkMode?: boolean;
+  mapPageUrl?: (id: string) => string;
+}
+
+const NotionRenderer: FC<Props> = ({ recordMap, darkMode, mapPageUrl, components, }) => {
+  const [scheme] = useScheme()
+
+  return (
+
+    <_NotionRenderer
+      darkMode={scheme === "dark"}
+      recordMap={recordMap}
+      components={{
+        ...components,
+        Block: CustomBlock, // CustomBlock 컴포넌트로 Block 컴포넌트 대체
+        Collection,
+        Code,
+        Equation,
+        Modal,
+        Pdf,
+        nextImage: Image,
+        nextLink: Link,
+      }}
+      mapPageUrl={mapPageUrl}
+
+    />
+
   )
 }
 
-export default PostDetail
+export default NotionRenderer
 
 const StyledWrapper = styled.div`
-    padding-left: 1.5rem;
-    padding-right: 1.5rem;
-    padding-top: 3rem;
-    padding-bottom: 3rem;
-    border-radius: 1.5rem;
-    max-width: 56rem;
-    background-color: ${({ theme }) =>
-            theme.scheme === "light" ? "white" : theme.colors.gray4};
-    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
-    0 2px 4px -1px rgba(0, 0, 0, 0.06);
-    margin: 0 auto;
-    > article {
-        margin: 0 auto;
-        max-width: 42rem;
+    /* // TODO: why render? */
+    .notion-collection-page-properties {
+        display: none !important;
     }
-`
+    .notion-page {
+        padding: 0;
+    }
+    .notion-list {
+        width: 100%;
+    }
+
+`;
