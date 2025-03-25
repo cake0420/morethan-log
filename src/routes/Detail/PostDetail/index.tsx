9a77c4ddeab97803ba4a3cfe21991bdc54277e77
soowan <nick6767676767@gmail.com>
2025-03-25 18:39:36 +0900

    slug css 수정 테스트

--- src/routes/Detail/PostDetail/index.tsx
+++ src/routes/Detail/PostDetail/index.tsx
@@ -28,7 +28,7 @@ const PostDetail: React.FC<Props> = () => {
         )}
         {data.type[0] === "Post" && <PostHeader data={data} />}
         <div>
-          <NotionRenderer recordMap={data.recordMap} />
+          <NotionRenderer recordMap={data.recordMap} components={{}}/> {/* components prop 추가 */}
         </div>
         {data.type[0] === "Post" && (
           <>
@@ -44,19 +44,19 @@ const PostDetail: React.FC<Props> = () => {
 export default PostDetail
 
 const StyledWrapper = styled.div`
-  padding-left: 1.5rem;
-  padding-right: 1.5rem;
-  padding-top: 3rem;
-  padding-bottom: 3rem;
-  border-radius: 1.5rem;
-  max-width: 56rem;
-  background-color: ${({ theme }) =>
-    theme.scheme === "light" ? "white" : theme.colors.gray4};
-  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+    padding-left: 1.5rem;
+    padding-right: 1.5rem;
+    padding-top: 3rem;
+    padding-bottom: 3rem;
+    border-radius: 1.5rem;
+    max-width: 56rem;
+    background-color: ${({ theme }) =>
+            theme.scheme === "light" ? "white" : theme.colors.gray4};
+    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
     0 2px 4px -1px rgba(0, 0, 0, 0.06);
-  margin: 0 auto;
-  > article {
     margin: 0 auto;
-    max-width: 42rem;
-  }
+    > article {
+        margin: 0 auto;
+        max-width: 42rem;
+    }
 `
